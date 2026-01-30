b433808d49dfc73073a1bca03dbb4fccecf5d59e
Nahid Liton <123287064+Nahidliton@users.noreply.github.com>
2026-01-30 07:34:59 +0600

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,16 @@
+void main() {
+  String Name = "Nahid";
+  print(Name);
+  nm();
+  db();
+}
+
+void nm() {
+  int Num = 10;
+  print(Num);
+}
+
+void db() {
+  double num = 10.56;
+  print(num);
+}
