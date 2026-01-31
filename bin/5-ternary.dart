814c56c6ff21f9310dc86c69ba81d3a1b17bd71a
Nahid Liton <123287064+Nahidliton@users.noreply.github.com>
2026-01-31 09:03:08 +0600

    update

---
+++ bin/5-ternary.dart
@@ -0,0 +1,16 @@
+/*void main() {
+  var a = 3;
+  var b = 4;
+  var maxNumber;
+  maxNumber = (a > b) ? a : b;
+  print(maxNumber);
+}
+*/
+
+void main() {
+  var a = 10;
+  var b = 100;
+  var maxNumber;
+  maxNumber = (a > b) ? a : b;
+  print(maxNumber);
+}
