fc27b01995dd0ff0ba7940754e06ed46385e3721
Nahid Liton <123287064+Nahidliton@users.noreply.github.com>
2026-01-31 09:51:32 +0600

    good

---
+++ bin/6-switch_case.dart
@@ -0,0 +1,22 @@
+void main(){
+    String Grade="A";
+    switch(Grade){
+      case "A":
+        print("Excellent");
+        break;
+      case "B":
+        print("Very Good");
+        break;
+      case "C":
+        print("Good");
+        break;
+      case "D":
+        print("Pass");
+        break;
+      case "F":
+        print("Fail");
+        break;
+      default:
+        print("Invalid Grade");
+    }
+}
