b0ab7ca95a6256bd59b13f9f31ff5d1d2b5e51f4
Nahid Liton <123287064+Nahidliton@users.noreply.github.com>
2026-02-02 00:32:16 +0600

    f

---
+++ bin/10-continue.dart
@@ -0,0 +1,8 @@
+void main() {
+  for (var i = 0; i < 20; i++) {
+    if (i == 10 && i == 12) {
+      continue;
+    }
+    print("Output = $i");
+  }
+}
--- bin/9-break.dart
+++ bin/9-break.dart
@@ -1,1 +1,8 @@
-void main() {}
+void main() {
+  for (var i = 0; i < 10; i++) {
+    if (i == 6) {
+      break;
+    }
+    print(i);
+  }
+}
