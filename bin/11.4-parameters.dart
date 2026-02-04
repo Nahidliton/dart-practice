f4e7bcec6b53ee2c1666d287fdb7140088cc9618
Nahid Liton <123287064+Nahidliton@users.noreply.github.com>
2026-02-05 00:17:38 +0600

    r

---
+++ bin/11.4-parameters.dart
@@ -0,0 +1,8 @@
+void main (){
+  city("Dhaka", "Chittagong", "Khulna");
+}
+void city(String city1, String city2, String city3) {
+  print("City 1 : $city1");
+  print("City 2 : $city2");
+  print("City 3 : $city3");
+}
